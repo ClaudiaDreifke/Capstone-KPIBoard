0afce2b7fa324e76feedd9c6949683fda36689a4
claudia <claudia.dreifke@gmail.com>
2022-08-18 11:16:45 +0200

    delete HelloController and modify app.tsx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,19 +1,10 @@
-import React, {useState} from 'react';
 import './App.css';
-import axios from "axios";
 
 export default function App() {
 
-    const [message, setMessage] = useState();
-
-    axios.get("/hello")
-        .then(response => response.data)
-        .then(setMessage)
-
-
     return (
         <>
-            <h1>{message}</h1>
+            <h1>test</h1>
         </>
     );
 }
