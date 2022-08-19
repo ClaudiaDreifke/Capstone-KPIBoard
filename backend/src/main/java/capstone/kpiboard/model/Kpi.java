7504fe7c38f57b15391b135ff78f2ef0514f7bc4
claudia <claudia.dreifke@gmail.com>
2022-08-19 11:29:02 +0200

    delete @Data from KPI class

--- backend/src/main/java/capstone/kpiboard/model/Kpi.java
+++ backend/src/main/java/capstone/kpiboard/model/Kpi.java
@@ -1,12 +1,10 @@
 package capstone.kpiboard.model;
 
 import lombok.Builder;
-import lombok.Data;
 import org.springframework.data.annotation.Id;
 
 import java.util.List;
 
-@Data
 @Builder
 public class Kpi {
     @Id
