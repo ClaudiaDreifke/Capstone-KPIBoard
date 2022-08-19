dd580cf74f83cfdf311449631d564ad81b8bf282
claudia <claudia.dreifke@gmail.com>
2022-08-19 09:05:33 +0200

    add application properties to test

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,16 +1,10 @@
-import React, {useState} from 'react';
 import './App.css';
-import axios from "axios";
 
 export default function App() {
 
-    const [message, setMessage] = useState();
-
-    axios.get("/hello")
-        .then(response => response.data)
-        .then(setMessage)
-
-  return (
-   <h1>{message}</h1>
-  );
+    return (
+        <>
+            <h1>KPI-Board</h1>
+        </>
+    );
 }
