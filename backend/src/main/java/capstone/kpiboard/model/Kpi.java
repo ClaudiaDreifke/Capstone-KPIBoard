f857c4f16ab81552a9f126ecb8ac1a3827fa86ce
claudia <claudia.dreifke@gmail.com>
2022-08-19 11:33:29 +0200

    add @Data from KPI class

--- backend/src/main/java/capstone/kpiboard/model/Kpi.java
+++ backend/src/main/java/capstone/kpiboard/model/Kpi.java
@@ -1,10 +1,12 @@
 package capstone.kpiboard.model;
 
 import lombok.Builder;
+import lombok.Data;
 import org.springframework.data.annotation.Id;
 
 import java.util.List;
 
+@Data
 @Builder
 public class Kpi {
     @Id
