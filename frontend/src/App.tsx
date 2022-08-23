7b2e70eb3dd639a0fd69ec7ca870c23054e2f386
claudia <claudia.dreifke@gmail.com>
2022-08-23 16:20:52 +0200

    resolve merging problems

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,14 +2,10 @@ import React from "react";
 import {HashRouter} from "react-router-dom";
 import AllRoutes from "./components/AllRoutes";
 import Header from "./components/Header";
-import AddKpi from "./components/AddKpi";
 import {toast, ToastContainer} from "react-toastify";
-import useKpi from "./hooks/useKpi";
 import './styling/App.css'
 
 export default function App() {
-
-    const {kpis, addNewKpi} = useKpi();
 
     return (
         <>
--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -15,7 +15,7 @@ export default function Header() {
             <nav className={"menu"}>
                 <NavLink className={"nav"} onClick={() => setTab("Home")} to={"/home"}> home </NavLink>
                 <NavLink className={"nav"} onClick={() => setTab("myKpi")} to={"/my-kpi"}> myKpi </NavLink>
-                <NavLink className={"nav"} onClick={() => setTab("Admin")} to={"/kpi-board"}> admin </NavLink>
+                <NavLink className={"nav"} onClick={() => setTab("Admin")} to={"/admin"}> admin </NavLink>
                 <NavLink className={"nav"} onClick={() => setTab("Logout")} to={"/"}> logout </NavLink>
             </nav>
             <img src={"pictures/coffee-lover-small.png"} className={"coffee-lover"} alt={""}/>
