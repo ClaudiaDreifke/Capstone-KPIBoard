bf97d6d4e1ed6aec42af2178d7bedfc26d232f40
ClaudiaDreifke <108395674+ClaudiaDreifke@users.noreply.github.com>
2022-08-27 13:43:44 +0200

    harmonize on change methods

--- frontend/src/components/Admin/ChangeKpiAdmin.tsx
+++ frontend/src/components/Admin/ChangeKpiAdmin.tsx
@@ -1,9 +1,9 @@
-import {FormControl, FormGroup, InputLabel, MenuItem, Select, SelectChangeEvent, TextField} from "@mui/material";
+import {FormControl, FormGroup, InputLabel, MenuItem, Select, TextField} from "@mui/material";
 import {FormEvent, useEffect, useState} from "react";
 import {toast} from "react-toastify";
 import {useNavigate, useParams} from "react-router-dom";
-import {Kpi} from "../model/Kpi";
-import '../styling/ChangeKpiAdmin.css'
+import {Kpi} from "../../model/Kpi";
+import '../../styling/ChangeKpiAdmin.css'
 
 export type ChangeKpiAdminProps = {
     kpis: Kpi[],
@@ -46,26 +46,18 @@ export default function ChangeKpiAdmin(props: ChangeKpiAdminProps) {
         navigate("/admin")
     }
 
-    const handleSelectTargetValueOperatorChange = (event: SelectChangeEvent) => {
-        setTargetValueOperator(event.target.value)
-    }
-
-    const handleSelectTargetValueUnitChange = (event: SelectChangeEvent) => {
-        setTargetValueUnit(event.target.value)
-    }
-
     return (
         <>
             <form onSubmit={onKpiSubmit}>
                 <h3>Kennzahl ändern</h3>
-                <p id="name">{kpi?.name}</p>
+                <p className={"name"} id="name">{kpi?.name}</p>
                 <FormControl sx={{m: 1, minWidth: 80}}>
                     <InputLabel id="target-value-operator"></InputLabel>
                     <Select
                         labelId="target-value-operator"
                         id="target-value-operator"
                         value={targetValueOperator}
-                        onChange={handleSelectTargetValueOperatorChange}>
+                        onChange={event => setTargetValueOperator(event.target.value)}>
                         <MenuItem value={"GREATER"}>größer</MenuItem>
                         <MenuItem value={"LESS"}>kleiner</MenuItem>
                         <MenuItem value={"EQUALS"}>gleich</MenuItem>
@@ -83,7 +75,7 @@ export default function ChangeKpiAdmin(props: ChangeKpiAdminProps) {
                         labelId="target-value-unit"
                         id="target-value-unit"
                         value={targetValueUnit}
-                        onChange={handleSelectTargetValueUnitChange}>
+                        onChange={event => setTargetValueUnit(event.target.value)}>
                         <MenuItem value={"ANZAHL"}>Anzahl</MenuItem>
                         <MenuItem value={"PROZENT"}>%</MenuItem>
                     </Select>
