b2f18217afe4834fc104ea42b181b4fbd499a96e
ClaudiaDreifke <108395674+ClaudiaDreifke@users.noreply.github.com>
2022-08-29 14:16:05 +0200

    add page for updating values

--- frontend/src/components/User/ChangeKpiUser.tsx
+++ frontend/src/components/User/ChangeKpiUser.tsx
@@ -1,4 +1,6 @@
 import {Kpi} from "../../model/Kpi";
+import {useNavigate, useParams} from "react-router-dom";
+import {FormGroup} from "@mui/material";
 
 export type ChangeKpiUserProps = {
     kpis: Kpi[],
@@ -7,8 +9,32 @@ export type ChangeKpiUserProps = {
 
 export default function ChangeKpiUser(props: ChangeKpiUserProps) {
 
+    const navigate = useNavigate();
+    const {id} = useParams();
+    const kpi: Kpi | undefined = props.kpis.find((k: Kpi) => k.id === id);
+
+    const targetValueOperatorToText = () => {
+        if (kpi?.targetForKpi.targetValueOperator === "LESS") return <>kleiner</>;
+        if (kpi?.targetForKpi.targetValueOperator === "GREATER") return <>größer</>;
+        else return <>gleich</>;
+    }
+
+    const targetValueUnitToText = () => {
+        if (kpi?.targetForKpi.targetValueUnit === "ANZAHL") return <> Stk.</>;
+        else return <>%</>;
+    }
+
     return (
         <>
+            <h3>Kennzahl ändern</h3>
+            <FormGroup style={{flex: 2, flexDirection: "row", marginLeft: 10, justifyContent: "start"}}>
+                <p>{kpi?.name}</p>
+                <p style={{marginLeft: 20}}> Zielwert: {targetValueOperatorToText()} {kpi?.targetForKpi.targetValue} {targetValueUnitToText()}</p>
+            </FormGroup>
+            <FormGroup style={{flex: 2, flexDirection: "row", marginLeft: 20, justifyContent: "flex-start"}}>
+                <button id={"back-to-admin-view"} onClick={() => navigate("/my-kpi")}>zurück</button>
+                <button type={"submit"}>ändern</button>
+            </FormGroup>
         </>
     )
 }
