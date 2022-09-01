304ed8b209151ef82f5b060226b074110a0e5464
ClaudiaDreifke <108395674+ClaudiaDreifke@users.noreply.github.com>
2022-09-01 10:25:18 +0200

    add compare method to fill compared list when updating kpi

--- backend/src/main/java/capstone/kpiboard/service/KpiService.java
+++ backend/src/main/java/capstone/kpiboard/service/KpiService.java
@@ -1,11 +1,10 @@
 package capstone.kpiboard.service;
 
 import capstone.kpiboard.exceptions.KpiNotFoundException;
-import capstone.kpiboard.model.Kpi;
-import capstone.kpiboard.model.NewKpi;
-import capstone.kpiboard.model.TargetForKpi;
+import capstone.kpiboard.model.*;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Service
@@ -33,8 +32,42 @@ public class KpiService {
 
     public Kpi updateKpiById(Kpi updatedKpi) {
         if (kpiRepo.existsById(updatedKpi.id())) {
-            return kpiRepo.save(new Kpi(updatedKpi.id(), updatedKpi.name(), updatedKpi.values(), updatedKpi.comparedValues(), new TargetForKpi(
+            return kpiRepo.save(new Kpi(updatedKpi.id(), updatedKpi.name(), updatedKpi.values(), compareValuesWithTarget(updatedKpi), new TargetForKpi(
                     updatedKpi.targetForKpi().targetValueOperator(), updatedKpi.targetForKpi().targetValue(), updatedKpi.targetForKpi().targetValueUnit())));
         } else throw new KpiNotFoundException(updatedKpi.id());
+    }
+
+    public List<ComparedMonthValuePair> compareValuesWithTarget(Kpi kpiToCompare) {
+        double actualTargetValue = kpiToCompare.targetForKpi().targetValue();
+        String actualTargetValueOperator = kpiToCompare.targetForKpi().targetValueOperator().toString();
+
+        List<MonthValuePair> listToCompare = kpiToCompare.values();
+        List<ComparedMonthValuePair> comparedValues = new ArrayList<>();
+
+        listToCompare.forEach(v -> {
+                    if (actualTargetValueOperator.equals("GREATER")) {
+                        if (v.value() > actualTargetValue) {
+                            comparedValues.add(new ComparedMonthValuePair(v.month(), 1));
+                        } else {
+                            comparedValues.add(new ComparedMonthValuePair(v.month(), 0));
+                        }
+                    }
+                    if (actualTargetValueOperator.equals("LESS")) {
+                        if (v.value() < actualTargetValue) {
+                            comparedValues.add(new ComparedMonthValuePair(v.month(), 1));
+                        } else {
+                            comparedValues.add(new ComparedMonthValuePair(v.month(), 0));
+                        }
+                    }
+                    if (actualTargetValueOperator.equals("EQUALS")) {
+                        if (v.value() == actualTargetValue) {
+                            comparedValues.add(new ComparedMonthValuePair(v.month(), 1));
+                        } else {
+                            comparedValues.add(new ComparedMonthValuePair(v.month(), 0));
+                        }
+                    }
+                }
+        );
+        return comparedValues;
     }
 }
