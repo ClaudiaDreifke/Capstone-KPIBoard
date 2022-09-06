57741c19eb25d151addc6a1b4370550aad7cc239
ClaudiaDreifke <108395674+ClaudiaDreifke@users.noreply.github.com>
2022-09-06 10:34:30 +0200

    correct exception in RoleService

--- backend/src/main/java/capstone/kpiboard/service/role/RoleService.java
+++ backend/src/main/java/capstone/kpiboard/service/role/RoleService.java
@@ -1,6 +1,5 @@
 package capstone.kpiboard.service.role;
 
-import capstone.kpiboard.exceptions.KpiNotFoundException;
 import capstone.kpiboard.exceptions.RoleNotFoundException;
 import capstone.kpiboard.model.role.NewRole;
 import capstone.kpiboard.model.role.Role;
@@ -28,6 +27,6 @@ public class RoleService {
     public void deleteRoleById(String id) throws RoleNotFoundException {
         if (roleRepo.existsById(id)) {
             roleRepo.deleteById(id);
-        } else throw new KpiNotFoundException(id);
+        } else throw new RoleNotFoundException(id);
     }
 }
