7cfb2be716aa3a840497c7fb20afb5f5f12991e4
ClaudiaDreifke <108395674+ClaudiaDreifke@users.noreply.github.com>
2022-09-10 11:13:34 +0200

    add footer and modify website styling

---
+++ frontend/src/components/shared/Footer.tsx
@@ -0,0 +1,12 @@
+import '../../styling/Footer.css';
+
+
+export default function Footer() {
+
+    return (
+        <footer id={"stickyFooter"} className={"footer"}>
+            <title>EASY KPI</title>
+            <p className={"footer-text"}>EASY KPI ©2022</p>
+        </footer>
+    )
+}
