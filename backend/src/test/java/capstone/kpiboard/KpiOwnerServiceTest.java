125d285eb90154f820ef561e722a2d3898926b51
ClaudiaDreifke <108395674+ClaudiaDreifke@users.noreply.github.com>
2022-09-16 10:12:43 +0200

    add test for javax validation for new KpiOwner

--- backend/src/test/java/capstone/kpiboard/KpiOwnerServiceTest.java
+++ backend/src/test/java/capstone/kpiboard/KpiOwnerServiceTest.java
@@ -5,20 +5,32 @@ import capstone.kpiboard.model.roles.KpiOwner;
 import capstone.kpiboard.model.roles.NewKpiOwner;
 import capstone.kpiboard.service.owner.KpiOwnerRepo;
 import capstone.kpiboard.service.owner.KpiOwnerService;
+import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.stereotype.Service;
 
+import javax.validation.ConstraintViolation;
+import javax.validation.Validation;
+import javax.validation.Validator;
+import javax.validation.ValidatorFactory;
 import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
 
+import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.*;
 
+@Slf4j
 @Service
 class KpiOwnerServiceTest {
 
     private final KpiOwnerRepo testKpiOwnerRepo = mock(KpiOwnerRepo.class);
     private final KpiOwnerService testKpiOwnerService = new KpiOwnerService(testKpiOwnerRepo);
+
+    ValidatorFactory factory = Validation.buildDefaultValidatorFactory();
+    Validator validator = factory.getValidator();
 
     @Test
     void addNewKpiOwnerTest() {
@@ -30,6 +42,19 @@ class KpiOwnerServiceTest {
         KpiOwner actual = testKpiOwnerService.addNewKpiOwner(newTestRole);
         //then
         Assertions.assertEquals(testRole, actual);
+    }
+
+    @Test
+    void addNewKpiOwnerFailsWhenValidationFails() {
+        //given
+        NewKpiOwner newFailRole = new NewKpiOwner(null);
+
+        //when
+        Set<ConstraintViolation<NewKpiOwner>> violations = validator.validate(newFailRole);
+        log.info(violations.stream().map(v -> v.getPropertyPath() + ": " + v.getInvalidValue() + ": " + v.getMessage())
+                .collect(Collectors.joining("\n")));
+        //then
+        assertFalse(violations.isEmpty());
     }
 
     @Test
